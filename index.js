d6f566d3f67c7501e8ea1ccd9be3e913a8df078a
da vinci <84037196+daxvinci@users.noreply.github.com>
2024-08-16 15:23:09 +0100

    css not showing

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const letters = [];
 generateAlphabet()
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + 'public'))
+app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', async(req,res) => {
