50467192d751ef2e55f412895b5f4d809f305740
da vinci <84037196+daxvinci@users.noreply.github.com>
2024-08-16 13:49:52 +0100

    chane port

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import axios from 'axios'
 import bodyParser from 'body-parser'
-import { dirname } from "path";
+import { dirname} from "path";
 import { fileURLToPath } from "url";
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001
 const letters = [];
 generateAlphabet()
+
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
